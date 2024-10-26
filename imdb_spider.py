93411c86c4604c2e87c979a3a61c4d2bf56afba5
Bhagyashree Vishwakarma <167774877+Bhagyshree-Vishwakarma78288@users.noreply.github.com>
2024-10-26 14:44:34 +0530

    Add files via upload

---
+++ imdb_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class ImdbSpiderSpider(scrapy.Spider):
+    name = "imdb_spider"
+    allowed_domains = ["www.imdb.com"]
+    start_urls = ["https://www.imdb.com/chart/top/"]
+
+    def parse(self, response):
+        imdb_scrapes=response.css('div.ipc-metadata-list-summary-item__c')
+        for imdb_scrape in imdb_scrapes:
+            yield {
+                'title':imdb_scrape.css('a h3.ipc-title__text::text').get(),
+                 'Year':imdb_scrape.css('span.cli-title-metadata-item::text').get(),
+                 'Rating':imdb_scrape.css('span.ipc-rating-star--rating::text').get() 
+            }
+    
+
+  
+        
+         
+          
+
+
+            
